319c731e78ce076275bbe4578f879b5138215215
Shohboz <shohboztuit@gmail.com>
2023-07-29 20:00:01 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace project80
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * EEEEE
+             * DDDD
+             * CCC
+             * BB
+             * A
+             * 
+             * 
+             */
+            int n = 5;
+            for(int i =n+65; i>=65; i--)
+            {
+                for(int j = i; j<65+n; j++)
+                {
+                    Console.Write((char)i );
+                }
+                Console.WriteLine();
+            }
+       
+            
+        }
+    }
+}
